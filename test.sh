09d062ad013ae8f9b3038359246b4f727e739034
Julien <chouteau49@gmail.com>
2025-03-15 19:05:54 +0100

    feat: mise à jour de la version dans le script de test à la version 4

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "version : 3"
+echo "version : 4"
 echo "Le script de test a été exé
 cuté avec succès !"
 echo "C'est un test de la version 2."
 echo "C'est un test de la version 3."
+echo "C'est un test de la version 4."
